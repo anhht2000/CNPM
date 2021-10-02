9cb7825deedb06c61a8064b98e659da49319523d
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-10-02 20:47:18 +0700

    initial init project

---
+++ FRONTEND/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h2>Test webpack</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
